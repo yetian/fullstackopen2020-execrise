8a8946baa1c4bbc586fc4e9d4ed9efd914492836
YE TIAN <solaris.navi@gmail.com>
2020-06-01 20:40:14 +0200

    Added part2.1-2.5

---
+++ part2/2.1-2.5/src/index.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <p><strong>Total of {parts.map(p => p.exercises).reduce((cur, acc) => cur + acc)} exercises</strong></p>
+  )
+}
+
+const Course = ({courses}) => {
+  return (
+    <div>
+      {courses.map(course =>
+        <div key={course.id}>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return <Course courses={courses} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
